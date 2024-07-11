fd6f9ce9882059ca01239ba8f5e9e5ac1146ee29
Taehee Yoon <th.yoon.dev@gmail.com>
2024-07-11 23:35:30 +0900

    feat : add GetFlightIcaoData hook

---
+++ bi-haeng/src/app/api/static/index.ts
@@ -0,0 +1,4 @@
+export const FLIGHT_TRACKER_API_KEY =
+  process.env.NEXT_PUBLIC_FLIGHT_TRACKER_API_KEY;
+export const FLIGHT_TRACKER_API_BASE_URL =
+  "https://aviation-edge.com/v2/public";
---
+++ bi-haeng/src/entities/flights/flights.flightIcao.tsx
@@ -0,0 +1,72 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Aircraft {
+  iataCode: string;
+  icao24: string;
+  icaoCode: string;
+  regNumber: string;
+}
+
+interface Airline {
+  iataCode: string;
+  icaoCode: string;
+}
+
+interface Airport {
+  iataCode: string;
+  icaoCode: string;
+}
+
+interface Flight {
+  iataNumber: string;
+  icaoNumber: string;
+  number: string;
+}
+
+interface Geography {
+  altitude: number;
+  direction: number;
+  latitude: number;
+  longitude: number;
+}
+
+interface Speed {
+  horizontal: number;
+  isGround: number;
+  vspeed: number;
+}
+
+interface System {
+  squawk: string | null;
+  updated: number;
+}
+
+export interface FlightData {
+  aircraft: Aircraft;
+  airline: Airline;
+  arrival: Airport;
+  departure: Airport;
+  flight: Flight;
+  geography: Geography;
+  speed: Speed;
+  status: string;
+  system: System;
+}
+
+export const GetFlightIcaoData = () => {
+  const { data, isLoading, refetch } = useQuery<FlightData | undefined, Error>({
+    queryKey: ["getFlightIcaoData"],
+    queryFn: async () => {
+      const url = `/api/flights?flightIcao=APZ101`;
+      const response = await axios(url);
+      if (response.status !== 200) {
+        throw new Error("Network response was not ok");
+      }
+
+      return response.data as FlightData;
+    },
+  });
+
+  return { data, isLoading, refetch };
+};
